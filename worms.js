00eed726bf01e5f17b1e0bfa20372054a20a4a81
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2022-12-18 19:24:36 +0700

    Fixing

--- worms.js
+++ worms.js
@@ -1,154 +1,147 @@
-class Worms{
-    constructor(x, y, size){
-        this.x = x
-        this.y = y
-        this.size = size
-        this.tail = [{x:this.x, y:this.y}]
-        this.rotateX = 0
-        this.rotateY = 1
-  
-    }
-  
-    move(){
-        var newRect;
-        if(this.rotateX == 1){
-            newRect = {
-                x: this.tail[this.tail.length - 1].x + this.size,
-                y: this.tail[this.tail.length - 1].y
-            }
-        } else if(this.rotateX == -1){
-            newRect = {
-                x: this.tail[this.tail.length - 1].x - this.size,
-                y: this.tail[this.tail.length - 1].y
-            }
-        } else if(this.rotateY == 1){
-            newRect = {
-                x: this.tail[this.tail.length - 1].x,
-                y: this.tail[this.tail.length - 1].y + this.size
-            }
-        } else if(this.rotateY == -1){
-            newRect = {
-                x: this.tail[this.tail.length - 1].x,
-                y: this.tail[this.tail.length - 1].y - this.size
-            }
-        }
-  
-        this.tail.shift()
-        this.tail.push(newRect)
-    }
+class Worms {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.tail = [{ x: this.x, y: this.y }];
+    this.rotateX = 0;
+    this.rotateY = 1;
   }
-  
-  
-  class Apple{
-    constructor(){
-        console.log("apple")
-        console.log(worms.size)
-        var isTouching; 
-        while(true){
-            isTouching = false;
-            this.x = Math.floor(Math.random() * canvas.width / worms.size) * worms.size
-            this.y = Math.floor(Math.random() * canvas.height / worms.size) * worms.size
-            for(var i = 0; i < snake.tail.length;i++){
-                if(this.x == worms.tail[i].x && this.y == worms.tail[i].y){
-                    isTouching = true
-                }
-            }
-            console.log(this.x , this.y)
-            this.size = worms.size
-            this.color = "red"
-            if(!isTouching){
-                break;
-            }
+
+  move() {
+    var newRect;
+    if (this.rotateX == 1) {
+      newRect = {
+        x: this.tail[this.tail.length - 1].x + this.size,
+        y: this.tail[this.tail.length - 1].y,
+      };
+    } else if (this.rotateX == -1) {
+      newRect = {
+        x: this.tail[this.tail.length - 1].x - this.size,
+        y: this.tail[this.tail.length - 1].y,
+      };
+    } else if (this.rotateY == 1) {
+      newRect = {
+        x: this.tail[this.tail.length - 1].x,
+        y: this.tail[this.tail.length - 1].y + this.size,
+      };
+    } else if (this.rotateY == -1) {
+      newRect = {
+        x: this.tail[this.tail.length - 1].x,
+        y: this.tail[this.tail.length - 1].y - this.size,
+      };
+    }
+
+    this.tail.shift();
+    this.tail.push(newRect);
+  }
+}
+
+class Apple {
+  constructor() {
+    console.log("apple");
+    console.log(worms.size);
+    var isTouching;
+    while (true) {
+      isTouching = false;
+      this.x = Math.floor((Math.random() * canvas.width) / worms.size) * worms.size;
+      this.y = Math.floor((Math.random() * canvas.height) / worms.size) * worms.size;
+      for (var i = 0; i < worms.tail.length; i++) {
+        if (this.x == worms.tail[i].x && this.y == worms.tail[i].y) {
+          isTouching = true;
         }
+      }
+      console.log(this.x, this.y);
+      this.size = worms.size;
+      this.color = "red";
+      if (!isTouching) {
+        break;
+      }
     }
   }
-  
-  var canvas = document.getElementById("canvas")
-  
-  var worms = new Snake(20,20,20);
-  
-  var apple = new Apple();
-  
-  var canvasContext = canvas.getContext('2d');
-  
-  window.onload = ()=>{
-    gameLoop();
+}
+
+var canvas = document.getElementById("canvas");
+
+var worms = new Worms(20, 20, 20);
+
+var apple = new Apple();
+
+var canvasContext = canvas.getContext("2d");
+
+window.onload = () => {
+  gameLoop();
+};
+
+function gameLoop() {
+  setInterval(show, 1000 / 20); // here 15 is our fps value
+}
+
+function show() {
+  update();
+  draw();
+}
+
+function update() {
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  console.log("update");
+  worms.move();
+  eatApple();
+  checkHitWall();
+}
+
+function checkHitWall() {
+  var headTail = worms.tail[worms.tail.length - 1];
+  if (headTail.x == -worms.size) {
+    headTail.x = canvas.width - worms.size;
+  } else if (headTail.x == canvas.widh) {
+    headTail.x = 0;
+  } else if (headTail.y == -worms.size) {
+    headTail.y = canvas.height - worms.size;
+  } else if (headTail.y == canvas.height) {
+    headTail.y = 0;
   }
-  
-  function gameLoop(){
-    setInterval(show, 1000/20) // here 15 is our fps value
+}
+
+function eatApple() {
+  if (worms.tail[worms.tail.length - 1].x == apple.x && worms.tail[worms.tail.length - 1].y == apple.y) {
+    worms.tail[worms.tail.length] = { x: apple.x, y: apple.y };
+    apple = new Apple();
   }
-  
-  function show(){
-    update();
-    draw();
+}
+
+function draw() {
+  createRect(0, 0, canvas.width, canvas.height, "black");
+  createRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < worms.tail.length; i++) {
+    createRect(worms.tail[i].x + 2.5, worms.tail[i].y + 2.5, worms.size - 5, worms.size - 5, "white");
   }
-  
-  
-  function update(){
-    canvasContext.clearRect(0,0, canvas.width, canvas.height)
-    console.log("update")
-    worms.move()
-    eatApple()
-    checkHitWall();
-    
-  }
-  
-  function checkHitWall(){
-    var headTail = worms.tail[worms.tail.length -1]
-    if(headTail.x == - worms.size) {
-        headTail.x = canvas.width - worms.size
-    } else if(headTail.x == canvas.widh) {
-        headTail.x = 0
-    } else if(headTail.y == - worms.size) {
-        headTail.y = canvas.height - worms.size
-    } else if(headTail.y == canvas.height) {
-        headTail.y = 0 
+
+  canvasContext.font = "20px Arial";
+  canvasContext.fillStyle = "#00FF42";
+  canvasContext.fillText("Score: " + (worms.tail.length - 1), canvas.width - 120, 18);
+  createRect(apple.x, apple.y, apple.size, apple.size, apple.color);
+}
+
+function createRect(x, y, width, height, color) {
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, width, height);
+}
+
+window.addEventListener("keydown", (event) => {
+  setTimeout(() => {
+    if (event.keyCode == 37 && worms.rotateX != 1) {
+      worms.rotateX = -1;
+      worms.rotateY = 0;
+    } else if (event.keyCode == 38 && worms.rotateY != 1) {
+      worms.rotateX = 0;
+      worms.rotateY = -1;
+    } else if (event.keyCode == 39 && worms.rotateX != -1) {
+      worms.rotateX = 1;
+      worms.rotateY = 0;
+    } else if (event.keyCode == 40 && worms.rotateY != -1) {
+      worms.rotateX = 0;
+      worms.rotateY = 1;
     }
-  }
-  
-  function eatApple(){
-    if(worms.tail[worms.tail.length - 1].x == apple.x &&
-        worms.tail[worms.tail.length - 1].y == apple.y){
-            worms.tail[worms.tail.length] = {x:apple.x, y: apple.y}
-            apple = new Apple();
-        }
-  }
-  
-  function draw(){
-    createRect(0,0,canvas.width, canvas.height, "black")
-    createRect(0,0, canvas.width, canvas.height)
-    for(var i =0; i < worms.tail.length; i++){
-        createRect(snake.tail[i].x + 2.5, worms.tail[i].y + 2.5,
-            worms.size - 5, worms.size- 5, 'white')
-    }
-  
-    canvasContext.font = "20px Arial"
-    canvasContext.fillStyle = "#00FF42"
-    canvasContext.fillText("Score: "+ (worms.tail.length -1),canvas.width - 120, 18 );
-    createRect(apple.x, apple.y, apple.size, apple.size, apple.color)
-  }
-  
-  function createRect(x,y,width, height,color){
-    canvasContext.fillStyle = color
-    canvasContext.fillRect(x,y,width,height)
-  }
-  
-  window.addEventListener("keydown", (event)=>{
-    setTimeout(()=>{
-        if(event.keyCode == 37 && worms.rotateX != 1){
-            worms.rotateX = -1
-            worms.rotateY = 0;
-        } else if(event.keyCode == 38 && worms.rotateY != 1){
-            worms.rotateX = 0
-            worms.rotateY = -1;
-        }else if(event.keyCode == 39 && worms.rotateX != -1){
-            worms.rotateX = 1
-            worms.rotateY = 0;
-        }
-        else if(event.keyCode == 40 && worms.rotateY != -1){
-            worms.rotateX = 0
-            worms.rotateY = 1;
-        }
-    }, 1)
-  })
+  }, 1);
+});
